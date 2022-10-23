2a16b4b9f61e87dc30ab2d9ef550f52053fce10b
iouardi1 <ihssaneouardi2@gmail.com>
2022-10-24 00:10:35 +0100

    ex00 and ex01 are done, still need to work on ex02

--- module00/ex01/PhoneBook.cpp
+++ module00/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: iouardi <iouardi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 18:01:45 by iouardi           #+#    #+#             */
-/*   Updated: 2022/10/23 12:49:15 by iouardi          ###   ########.fr       */
+/*   Updated: 2022/10/23 23:39:39 by iouardi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,23 @@ PhoneBook::PhoneBook()
 	i = 0;
 }
 
+int	checkStrIsdigit(std::string str)
+{
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (!isdigit(str[i]))
+			return (0);
+	}
+	return (1);
+}
+
 void    PhoneBook::addContact()
 {
 	std::string str;
 
 	if (i == 8)
 		i = 0;
+	std::getline(std::cin, str);
 	do {	
 		std::cout << "Please enter the first name" << std::endl;
 		std::getline(std::cin, str);
@@ -48,7 +59,7 @@ void    PhoneBook::addContact()
 	do {
 		std::cout << "Please enter the phone number" << std::endl;
 		std::getline(std::cin, str);
-	} while(str.empty() || str == "");
+	} while(str.empty() || str == "" || !checkStrIsdigit(str));
 	contacts[i].setPhoneNumber(str);
 	do {
 		std::cout << "Please enter the darkest secret" << std::endl;
@@ -61,7 +72,31 @@ void    PhoneBook::addContact()
 void	PhoneBook::searchContact()
 {
 	int		i = 0;
-	std::cout << "index     /first name/last name /nickname  " << std::endl;
-	std::cout << std::setw(10);
-	std::cout << i << "/" << std::setw(10) << getContact(i).getFirstName() << "/" << std::endl;
+
+	std::cout << "     index|first name| last name|  nickname" << std::endl;
+	while (i < 8 && !getContact(i).getFirstName().empty())
+	{	std::cout << std::setw(10) << i << "|";
+		if (getContact(i).getFirstName().length() >= 10)
+			std::cout << getContact(i).getFirstName().substr(0, 9) + ".";
+		else
+			std::cout << std::setw(10) << getContact(i).getFirstName();
+		std::cout << "|";
+		if (getContact(i).getLastName().length() >= 10)
+			std::cout << getContact(i).getLastName().substr(0, 9) + ".";
+		else
+			std::cout << std::setw(10) << getContact(i).getLastName();
+		std::cout << "|";
+		if (getContact(i).getNickName().length() >= 10)
+			std::cout << getContact(i).getNickName().substr(0, 9) + "." << std::endl;
+		else
+			std::cout << std::setw(10) << getContact(i).getNickName() << std::endl;
+		i++;
+	}
+	std::cout << "Please enter the index you want" << std::endl;
+	std::cin >> i;
+	std::cout << "First name     : " << getContact(i).getFirstName() << std::endl;
+	std::cout << "Last name      : " << getContact(i).getLastName() << std::endl;
+	std::cout << "Nickname       : " << getContact(i).getNickName() << std::endl;
+	std::cout << "Phone number   : " << getContact(i).getPhoneNumber() << std::endl;
+	std::cout << "Darkest secret : " << getContact(i).getDarkestSecret() << std::endl;
 }
--- module00/ex01/main.cpp
+++ module00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: iouardi <iouardi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 17:36:02 by iouardi           #+#    #+#             */
-/*   Updated: 2022/10/23 12:28:26 by iouardi          ###   ########.fr       */
+/*   Updated: 2022/10/23 23:43:38 by iouardi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,13 @@ int main()
 
 	do {
 		std::cout << "Please enter your command : " << std::endl;
-		std::getline(std::cin, input);
+		std::cin >> input;
 		if (input ==  "ADD")
 			phoneBook.addContact();
 		else if (input == "SEARCH")
 			phoneBook.searchContact();
 		else if (input != "EXIT")
 			std::cout << "wrong command :(" << std::endl;
-		std::cout << phoneBook.getContact(0).getFirstName() << std::endl; 
 		}	while (input != "EXIT");
+	return (0);
 }
