187ed7421df19d1a316c0313d2fed21e47e14db5
Ihssane Ouardi <iouardi@e2r12p2.1337.ma>
2022-11-01 14:07:01 +0100

    ex04 is protected

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: iouardi <iouardi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 15:20:50 by iouardi           #+#    #+#             */
-/*   Updated: 2022/10/31 18:40:01 by iouardi          ###   ########.fr       */
+/*   Updated: 2022/11/01 14:06:11 by iouardi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,36 @@ int main(int ac , char **av)
 {
 	std::ifstream	myfile;
 	std::string		str;
-	std::string		str1(av[1]);
-	std::ofstream	myfile2(str1 + ".replace");
-	std::string		str2(av[2]);
-	std::string		str3(av[3]);
 	size_t			pos, pos2;
 
-	myfile.open(str1);
-	while (std::getline(myfile, str))
-	{
-		pos2 = 0;
-		do
-		{
-				pos = str.find(str2, pos2);
-				if (pos != std::string::npos)
-					str.erase(pos, str2.length()).insert(pos, str3);
-				pos2 = pos;
-		}	while (pos != std::string::npos);
-		myfile2 << str << std::endl;
+	if (ac == 4)
+	{	
+		std::string		str1(av[1]);
+		std::ofstream	myfile2(str1 + ".replace");
+		std::string		str2(av[2]);
+		std::string		str3(av[3]);
+		myfile.open(str1);
+		if (myfile.is_open())
+		{	
+			while (std::getline(myfile, str))
+			{
+				pos2 = 0;
+				do
+				{
+						pos = str.find(str2, pos2);
+						if (pos != std::string::npos)
+							str.erase(pos, str2.length()).insert(pos, str3);
+						pos2 = pos;
+				}	while (pos != std::string::npos);
+				myfile2 << str << std::endl;
+			}
+			myfile.close();
+			myfile2.close();
+		}
+		else
+			std::cout << "please enter a valid file\n";
 	}
+	else
+		std::cout << "please enter the right number of args\n";
 	
 }
