ac2fddba8897349635f80c22a383b7d7283eb7a8
iouardi1 <ihssaneouardi2@gmail.com>
2022-12-03 14:44:10 +0100

    ex01

---
+++ module05/ex01/Form.hpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iouardi <iouardi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/03 11:53:20 by iouardi           #+#    #+#             */
+/*   Updated: 2022/12/03 14:22:20 by iouardi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FORM_HPP
+#define FORM_HPP
+#include <iostream>
+#include <exception>
+#include "Bureaucrat.hpp"
+class Bureaucrat;
+
+class Form {
+
+	private:
+		const std::string	name;
+		bool				sign;
+		const	int			gradeSign;
+		const	int			gradeExec;
+	public:
+		Form();
+		~Form();
+		Form(const Form &copy);
+		Form(const std::string name, bool sign, const int gradeSign, const int gradeExec);
+		Form& operator=(const Form &copy);
+		const std::string getName() const;
+		bool	getSign() const;
+		const int&	getGradeSign() const;
+		const int&	getGradeExec() const;
+		class	GradeTooHighException: public std::exception
+		{
+			const	char *what() const throw();
+		};
+		class	GradeTooLowException: public std::exception
+		{
+			const char *what() const throw();
+		};
+		void	beSigned(Bureaucrat const &bureau);
+};
+
+std::ostream& operator<<(std::ostream& o, const Form& form);
+
+#endif
--- module05/ex01/main.cpp
+++ module05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: iouardi <iouardi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 23:56:36 by iouardi           #+#    #+#             */
-/*   Updated: 2022/12/03 11:49:53 by iouardi          ###   ########.fr       */
+/*   Updated: 2022/12/03 14:41:27 by iouardi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,14 @@
 int main()
 {
 	try {
-		Bureaucrat khtek("smito", 0);
+		Bureaucrat khtek("smito", 150);
+		Form khtek_lukhra("smithaa", 0, 150, 1);
 		std::cout << khtek;
-		khtek.decrementGrade();
-		khtek.incrementGrade();
-		std::cout << khtek;
+		std::cout << khtek_lukhra;
+		khtek_lukhra.beSigned(khtek);
+		// khtek.decrementGrade();
+		// khtek.incrementGrade();
+		// std::cout << khtek;
 	}
 	
 	catch(std::exception& e)
